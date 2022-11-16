57a41763b5f19ef24d7482397b7a19f97420c900
Shaviv Hoffman-Lowitz <33563242+Shaviv-Hoffman-Lowitz@users.noreply.github.com>
2022-11-15 20:10:29 -0800

    Update weatherbench.py

--- mmselfsup/datasets/data_sources/weatherbench.py
+++ mmselfsup/datasets/data_sources/weatherbench.py
@@ -1,1 +1,22 @@
+import mmcv
+import numpy as np
 
+from ..builder import DATASOURCES
+from .base import BaseDataSource
+
+
+@DATASOURCES.register_module()
+class WeatherbenchDataSource(BaseDataSource):
+
+    def load_annotations(self):
+
+        url = 'https://dataserv.ub.tum.de/s/m1524895/download?path=%2F5.625deg%2F2m_temperature&files=2m_temperature_5.625deg.zip'
+        download_and_extract_archive(
+          url,
+          self.data_prefix,
+          f
+      
+      
+        data_infos = []
+        # writing your code here.
+        return data_infos
