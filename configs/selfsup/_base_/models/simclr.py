849a3e4fa40f451b0f45abd98785941fa3d4cede
Pranay Shirodkar <pranaykar@hotmail.com>
2022-11-16 15:28:25 -0800

    change SyncBN to BN in SimCLR

--- configs/selfsup/_base_/models/simclr.py
+++ configs/selfsup/_base_/models/simclr.py
@@ -6,7 +6,7 @@ model = dict(
         depth=50,
         in_channels=3,
         out_indices=[4],  # 0: conv-1, x: stage-x
-        norm_cfg=dict(type='SyncBN'),
+        norm_cfg=dict(type='BN'),
         zero_init_residual=True),
     neck=dict(
         type='NonLinearNeck',  # SimCLR non-linear neck
