fd1b10fc114abd4fd0185684541540c426b3104e
Shaviv Hoffman-Lowitz <33563242+Shaviv-Hoffman-Lowitz@users.noreply.github.com>
2022-11-26 20:49:54 -0800

    Create simclr_unet.py

---
+++ configs/selfsup/_base_/models/simclr_unet.py
@@ -0,0 +1,16 @@
+# model settings
+model = dict(
+    type='SimCLR',
+    backbone=dict(
+        type='UNet',
+        in_channels=3,
+        norm_cfg=dict(type='BN'),
+        ),
+    neck=dict(
+        type='NonLinearNeck',  # SimCLR non-linear neck
+        in_channels=2048,
+        hid_channels=2048,
+        out_channels=128,
+        num_layers=2,
+        with_avg_pool=True),
+    head=dict(type='ContrastiveHead', temperature=0.1))
