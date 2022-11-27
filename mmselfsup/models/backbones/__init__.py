c9fb8268cb9603976867551e01ed0a18bc7d60c1
Shaviv Hoffman-Lowitz <33563242+Shaviv-Hoffman-Lowitz@users.noreply.github.com>
2022-11-26 20:41:20 -0800

    Update __init__.py

--- mmselfsup/models/backbones/__init__.py
+++ mmselfsup/models/backbones/__init__.py
@@ -7,6 +7,7 @@ from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .simmim_swin import SimMIMSwinTransformer
 from .vision_transformer import VisionTransformer
+from .unet import UNet
 
 __all__ = [
     'ResNet', 'ResNetV1d', 'ResNeXt', 'MAEViT', 'MIMVisionTransformer',
