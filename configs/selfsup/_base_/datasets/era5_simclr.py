f180a16a5c9f2964b055bf90058980bfd7360256
Michael Sherman <msherman32@gatech.edu>
2022-11-26 20:48:37 -0500

    removing several transformations

--- configs/selfsup/_base_/datasets/era5_simclr.py
+++ configs/selfsup/_base_/datasets/era5_simclr.py
@@ -3,21 +3,21 @@ data_source = 'EraDataSource'
 dataset_type = 'ERA5Dataset'
 img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 train_pipeline = [
-    dict(type='RandomResizedCrop', size=224),
+    # dict(type='RandomResizedCrop', size=224),
     dict(type='RandomHorizontalFlip'),
-    dict(
-        type='RandomAppliedTrans',
-        transforms=[
-            dict(
-                type='ColorJitter',
-                brightness=0.8,
-                contrast=0.8,
-                saturation=0.8,
-                hue=0.2)
-        ],
-        p=0.8),
-    dict(type='RandomGrayscale', p=0.2),
-    dict(type='GaussianBlur', sigma_min=0.1, sigma_max=2.0, p=0.5),
+    # dict(
+    #     type='RandomAppliedTrans',
+    #     transforms=[
+    #         dict(
+    #             type='ColorJitter',
+    #             brightness=0.8,
+    #             contrast=0.8,
+    #             saturation=0.8,
+    #             hue=0.2)
+    #     ],
+    #     p=0.8),
+    # dict(type='RandomGrayscale', p=0.2),
+    # dict(type='GaussianBlur', sigma_min=0.1, sigma_max=2.0, p=0.5),
 ]
 
 # prefetch
